26bc083c769b356f66371cd312f0c4fca94dd3dc
lupzbr <luiz@graficakyrios.com.br>
2015-09-15 15:04:13 -0300

    Create n18.java

---
+++ capitulo2/n18.java
@@ -0,0 +1,15 @@
+package br.com.kyrios.poo.exercicio.capitulo2;
+public class n18 {
+    public static void main(String [] args){
+        System.out.print("*********      ***         *          *     \n"
+                        +"*       *    *     *      ***        * *    \n"
+                        +"*       *   *       *    *****      *   *   \n"
+                        +"*       *   *       *      *       *     *  \n"
+                        +"*       *   *       *      *      *       * \n"
+                        +"*       *   *       *      *       *     *  \n"
+                        +"*       *   *       *      *        *   *   \n"
+                        +"*       *     *   *        *         * *    \n"
+                        +"*********      ***         *          *     \n"
+                        );          
+    }
+}
